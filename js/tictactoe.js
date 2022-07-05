54d62edf649b193f82eacac1d38fe5c59792fe6d
Christopher Holt <holt.christopher1@gmail.com>
2022-07-05 21:52:54 +1000

    Added images, js, css, board and players are working

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,1 +1,23 @@
-console.log('Is this on??');
+let hordeTurn;
+const playerTurn = ['hordeTurn', 'allianceTurn'];
+
+const swapTurn = function() {
+    hordeTurn = !hordeTurn;
+}
+
+$(document).ready(function () {
+    $('.cell').on('click', function() {
+        console.log('cell clicked');
+
+        if (hordeTurn) {
+            const img = $(this).addClass('horde');
+            $(this).attr('src', img);
+            swapTurn();
+        } 
+        else {
+            const img = $(this).addClass('alliance');
+            $(this).attr('src', img);
+            swapTurn();
+        } 
+    });
+});
